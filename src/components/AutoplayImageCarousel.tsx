d72127b8321438c044be99e347c3b97633cb9c58
Kamal Fasya <kamalfasya01@gmail.com>
2025-12-08 19:57:54 +0700

    fix: AutoplayImageCarousel works as expected

--- src/components/AutoplayImageCarousel.tsx
+++ src/components/AutoplayImageCarousel.tsx
@@ -1,12 +1,9 @@
-import { type ReactNode } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 import WheelGesturesPlugin from "embla-carousel-wheel-gestures";
@@ -26,19 +23,15 @@ export function AutoplayImageCarousel(props: {
         }),
         WheelGesturesPlugin({ forceWheelAxis: "x" }),
       ]}
-      className="w-full max-w-sm"
+      className="w-full max-w-5xl"
     >
       <CarouselContent>
-        {props.imgList.map((i, index) => (
+        {props.imgList.map((img, index) => (
           <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                                    <img src={i} alt="" />
-                  {/* <span className="text-3xl font-semibold">{index + 1}</span> */}
+              <Card className="p-0 overflow-hidden">
+                <CardContent className={cn(`aspect-square bg-cover bg-no-repeat bg-center bg-[url(${img})]`)}>
                 </CardContent>
               </Card>
-            </div>
           </CarouselItem>
         ))}
       </CarouselContent>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,11 @@ export function cn(...inputs: ClassValue[]) {
 export function randomClassId() {
   return Math.random().toString(36).slice(2, -1);
 }
+
+export function extractSourcePathRegex(pathString: string): string | null {
+  const regex = /src\/(.+?)(\?|$)/; 
+  
+  const match = pathString.match(regex);
+
+  return match ? match[1] : "";
+}
