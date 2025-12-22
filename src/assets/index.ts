60abb489f5b04e83983d527a2aaff3e5ced44031
Kamal Fasya <kamalfasya01@gmail.com>
2025-12-22 10:00:12 +0700

    feat: image for bounce card

---
+++ src/assets/index.ts
@@ -0,0 +1,21 @@
+import anggotaAll from "./1.webp";
+import anggotaBph from "./2.webp";
+import anggotaInovasi from "./3.webp";
+import anggotaMedia from "./4.webp";
+import anggotaMinat from "./5.webp";
+
+const allImages = [
+  anggotaAll,
+  anggotaBph,
+  anggotaInovasi,
+  anggotaMedia,
+  anggotaMinat,
+];
+export {
+  allImages,
+  anggotaAll,
+  anggotaBph,
+  anggotaInovasi,
+  anggotaMedia,
+  anggotaMinat,
+};
