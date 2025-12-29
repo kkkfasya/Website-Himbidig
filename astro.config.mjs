2c8c5b124b65ef7b19d15cbbf16e01189dda2e02
Kamal Fasya <kamalfasya01@gmail.com>
2025-12-30 03:59:25 +0700

    feat: add github workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://kkkfasya.github.io",
+  base: "/Website-Himbidig",
   vite: {
     plugins: [tailwindcss()],
   },
